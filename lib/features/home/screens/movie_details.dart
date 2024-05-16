981abd5f91a81c04f93f6fea0c5f4d916abc05ce
SumanRasaili <rasailisuman123@gmail.com>
2024-05-16 22:33:48 +0545

    movie data fetched and displayed

--- lib/features/home/screens/movie_details.dart
+++ lib/features/home/screens/movie_details.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:tmdbapp/config/app_constants.dart';
+import 'package:tmdbapp/features/home/models/movie_details_response.dart';
 import 'package:tmdbapp/features/home/providers/movie_details_provider.dart';
 
 class MovieDetailsScreen extends ConsumerWidget {
@@ -30,10 +31,10 @@ class MovieDetailsScreen extends ConsumerWidget {
             return SizedBox(
               height: MediaQuery.of(context).size.height,
               child: Stack(
-                fit: StackFit.loose,
+                // fit: StackFit.loose,
                 children: [
                   Positioned(
-                    height: MediaQuery.of(context).size.height * .45,
+                    height: MediaQuery.of(context).size.height * .40,
                     child: CachedNetworkImage(
                         fit: BoxFit.cover,
                         imageUrl:
@@ -99,20 +100,83 @@ class MovieDetailsScreen extends ConsumerWidget {
                         ],
                       )),
                   Positioned(
-                    bottom: 5,
-                    top: MediaQuery.of(context).size.height * 0.4,
+                    // bottom: 5,
+                    top: MediaQuery.of(context).size.height * 0.40,
                     child: Container(
+                      padding: const EdgeInsets.all(15),
                       width: MediaQuery.of(context).size.width,
                       decoration: const BoxDecoration(
-                          color: Colors.amber,
+                          // color: Colors.grey,
                           borderRadius: BorderRadius.only(
                               topLeft: Radius.circular(15),
                               topRight: Radius.circular(15))),
                       // width: constraints.maxWidth,
                       height: MediaQuery.of(context).size.height,
                       child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text(movieData.title),
+                          Row(
+                            children: [
+                              for (var cont in movieData.originCountry)
+                                Text(
+                                    "${movieData.status} (${movieData.originalLanguage} $cont)"),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              Text("${movieData.runtime} min"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Wrap(
+                            spacing: 5,
+                            runSpacing: 5.0,
+                            children: [
+                              for (var genre in movieData.genres)
+                                GenreWidget(genre)
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                "Budget : ",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              Text("\$${movieData.budget}"),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                "Earning : ",
+                                style: TextStyle(fontSize: 15),
+                              ),
+                              Text("\$${movieData.revenue}"),
+                            ],
+                          ),
+                          const Divider(),
+                          const Text(
+                            "Overview : ",
+                            style: TextStyle(fontSize: 15),
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          Text(
+                            movieData.overview,
+                            style: TextStyle(
+                                fontSize: 12, color: Colors.grey.shade800),
+                          ),
+                          const Divider(),
+                          Production(movieData.productionCompanies),
+                          const Divider(),
                         ],
                       ),
                     ),
@@ -137,5 +201,95 @@ class MovieDetailsScreen extends ConsumerWidget {
         ]))
       ],
     ));
+  }
+}
+
+class GenreWidget extends StatelessWidget {
+  final Genre genre;
+  const GenreWidget(this.genre, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Theme.of(context).primaryColor.withOpacity(0.3)),
+      child: Text(
+        genre.name.toUpperCase(),
+        style: Theme.of(context).textTheme.bodySmall,
+      ),
+    );
+  }
+}
+
+class Production extends StatelessWidget {
+  final List<ProductionCompany> productionCompanies;
+  const Production(this.productionCompanies, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Production',
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        SizedBox(
+          height: 80,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: [
+              for (var each in productionCompanies)
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: ProducerWidget(each),
+                )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ProducerWidget extends StatelessWidget {
+  final ProductionCompany productionCompany;
+  const ProducerWidget(this.productionCompany, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        productionCompany.logoPath.isNotEmpty
+            ? Container(
+                height: 50,
+                width: 50,
+                decoration: const BoxDecoration(shape: BoxShape.circle),
+                child: Text(
+                  productionCompany.name[0].toUpperCase(),
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              )
+            : Container(
+                height: 50,
+                width: 50,
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(AppConstants.imageBaseUrl +
+                            productionCompany.logoPath))),
+              ),
+        Text(
+          productionCompany.name,
+          style: Theme.of(context).textTheme.bodySmall,
+        )
+      ],
+    );
   }
 }
