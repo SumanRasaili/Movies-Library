b096ec85dfe10508cdec719054ba521447c71a25
SumanRasaili <rasailisuman123@gmail.com>
2024-05-18 16:32:32 +0545

    timer enabled while search

--- lib/features/home/screens/search_page.dart
+++ lib/features/home/screens/search_page.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
@@ -18,6 +20,8 @@ class SearchPage extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     final textEditingController = useTextEditingController();
     final searchProv = ref.watch(searchProvider);
+    final isSearching = useState<bool>(false);
+    final timer = useState<Timer?>(null);
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -26,45 +30,70 @@ class SearchPage extends HookConsumerWidget {
             title: TextFormField(
               controller: textEditingController,
               onChanged: (value) {
-                ref.read(searchProvider.notifier).getData(searchQuery: value);
+                isSearching.value = true;
+                if (timer.value != null) {
+                  timer.value?.cancel();
+                }
+                timer.value = Timer(const Duration(seconds: 1), () async {
+                  ref.read(searchProvider.notifier).getData(searchQuery: value);
+                  isSearching.value = false;
+                });
               },
               onFieldSubmitted: (value) {
                 ref.read(searchProvider.notifier).getData(searchQuery: value);
               },
-              decoration: const InputDecoration(
-                  suffixIcon: Icon(
-                    Icons.search,
-                    size: 25,
-                  ),
+              decoration: InputDecoration(
+                  suffixIcon: isSearching.value
+                      ? const Icon(
+                          Icons.search,
+                          size: 25,
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            textEditingController.clear();
+                            textEditingController.text = "";
+                          },
+                          child: const Icon(
+                            Icons.clear,
+                            size: 25,
+                          ),
+                        ),
                   hintText: "Search here",
                   isDense: true,
-                  contentPadding: EdgeInsets.fromLTRB(12, 11, 12, 11),
-                  border: OutlineInputBorder(
+                  contentPadding: const EdgeInsets.fromLTRB(12, 11, 12, 11),
+                  border: const OutlineInputBorder(
                       borderRadius: BorderRadius.all(Radius.circular(15)))),
             ),
           ),
-          SliverGrid.builder(
-            gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 250,
-              childAspectRatio: 0.96,
-              crossAxisSpacing: 8,
-              mainAxisSpacing: 8,
-              // childAspectRatio: ,
+          if (isSearching.value)
+            const SliverToBoxAdapter(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            )
+          else
+            SliverGrid.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 250,
+                childAspectRatio: 0.96,
+                crossAxisSpacing: 8,
+                mainAxisSpacing: 8,
+                // childAspectRatio: ,
+              ),
+              itemCount: searchProv.searchResultsList.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(left: 15, right: 15),
+                  child: CardWidget(
+                      model: searchProv.searchResultsList[index],
+                      onPressed: () {
+                        context.push(MovieDetailsScreen.routename,
+                            extra: searchProv.searchResultsList[index].id
+                                .toString());
+                      }),
+                );
+              },
             ),
-            itemCount: searchProv.searchResultsList.length,
-            itemBuilder: (context, index) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15),
-                child: CardWidget(
-                    model: searchProv.searchResultsList[index],
-                    onPressed: () {
-                      context.push(MovieDetailsScreen.routename,
-                          extra: searchProv.searchResultsList[index].id
-                              .toString());
-                    }),
-              );
-            },
-          ),
         ],
       ),
     );
